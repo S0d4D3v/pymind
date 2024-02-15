dbc252fb6c9fd49ec0b3d6d2646f63c5eb4d039f
SodaDev <sodafodd@free.fr>
2024-02-15 18:40:33 +0100

    Upd. to Alpha 1

--- src/funcs.py
+++ src/funcs.py
@@ -1,5 +1,20 @@
 import time
 
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    PINK = '\033[35m'
+
 def check_guess(guess, answer) :
     index = 0
     ans = []
