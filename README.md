9034003812e5dafa7f434ac387c9ade501ab325a
SodaDev <sodafodd@free.fr>
2024-02-15 19:47:23 +0100

    Update files & added README

---
+++ README.md
@@ -0,0 +1,31 @@
+# Mastermind
+## This is a simple mastermind game in python to run in the CLI
+
+# How to install
+First, download this repository as a zip file or clone it by using the following command :
+```sh
+git clone https://github.com/S0d4D3v/mastermind.git
+```
+If you chose to download it as a zip file, extract it.
+Execute **index** by opening it or by running the following command :
+```sh
+python3.12 index
+```
+
+# Game rules
+You will have to find a series of colors to find, with as the options :
+**R** for *red*, 
+**O** for *orange*, 
+**Y** for *yellow*, 
+**G** for *green*, 
+**C** for *cyan*, 
+**S** for *sky*, 
+**B** for *blue* 
+and **P** for *pink*
+
+The list below shows the difficulties you will have to chose before starting the game :
+| Level : | *Easy* | *Normal* | *Hard* | *Extreme* | *Ultimate* |
+|-|-|-|-|-|-|
+| Trials : | 15 | 15 | 15 | 12 | 10
+| Colors to find : | 4 | 4 | 5 | 5 | 6
+| Possible colors : | *R G B P* | *R O Y G B P* | *R O Y G B P* | *R O Y G B P* | *R O Y G C S B P* 
--- src/funcs.py
+++ src/funcs.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 class colors:
     HEADER = '\033[95m'
@@ -14,6 +15,36 @@ class colors:
     YELLOW = '\033[33m'
     BLUE = '\033[34m'
     PINK = '\033[35m'
+
+def randomize_answer(level) :
+    if level == 1 :
+        answer = [ str(random.randint(1, 4)),  str(random.randint(1, 4)), str(random.randint(1, 4)), str(random.randint(1, 4))]
+    elif level == 2 :
+        answer = [ str(random.randint(1, 6)),  str(random.randint(1, 6)), str(random.randint(1, 6)), str(random.randint(1, 6))]
+    elif level == 3 or level == 4 :
+        answer = [ str(random.randint(1, 6)),  str(random.randint(1, 6)), str(random.randint(1, 6)), str(random.randint(1, 6)), str(random.randint(1, 6))]
+    elif level == 5 :
+        answer = [ str(random.randint(1, 8)),  str(random.randint(1, 8)), str(random.randint(1, 8)), str(random.randint(1, 8)), str(random.randint(1, 8)), str(random.randint(1, 8))]
+    index = 0
+    while index < len(answer) :
+        if answer[index] == '1' :
+            answer[index] = 'R'
+        elif answer[index] == '2' :
+            answer[index] = 'G'
+        elif answer[index] == '3' :
+            answer[index] = 'B'
+        elif answer[index] == '4' :
+            answer[index] = 'P'
+        elif answer[index] == '5' :
+            answer[index] = 'Y'
+        elif answer[index] == '6' :
+            answer[index] == 'W'
+        elif answer[index] == '7' :
+            answer[index] == 'C'
+        elif answer[index] == '8' :
+            answer[index] == 'S'
+        index += 1
+    return answer
 
 def check_guess(guess, answer) :
     index = 0
