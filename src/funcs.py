b9566a31d979459eb9d9880b5433a3a520af39ef
SodaDev <sodafodd@free.fr>
2024-02-16 14:50:56 +0100

    Updated files and (almost) fixed interface

--- src/funcs.py
+++ src/funcs.py
@@ -1,21 +1,6 @@
 import time
 import random
 
-class colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    PINK = '\033[35m'
-
 def randomize_answer(level) :
     if level == 1 :
         answer = [ str(random.randint(1, 4)),  str(random.randint(1, 4)), str(random.randint(1, 4)), str(random.randint(1, 4))]
@@ -46,21 +31,54 @@ def randomize_answer(level) :
         index += 1
     return answer
 
-def check_guess(guess, answer) :
+def check_guess(level, guess, answer) :
     index = 0
     ans = []
     while index < len(answer) :
-        if guess[index] == answer[index] :
-            ans += [colors.OKGREEN, str(guess[index])]
-        elif guess[index] == answer[0] :
-            ans += [colors.WARNING, str(guess[index])]
-        elif guess[index] == answer[1] :
-            ans += [colors.WARNING, str(guess[index])]
-        elif guess[index] == answer[2] :
-            ans += [colors.WARNING, str(guess[index])]
-        elif guess[index] == answer[3] :
-            ans += [colors.WARNING, str(guess[index])]
-        else :
-            ans += [colors.FAIL, str(guess[index])]
+        if level <= 1 :
+            if guess[index] == answer[index] :
+                ans += ["\033[92m", str(guess[index])]
+            elif guess[index] == answer[0] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[1] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[2] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[3] :
+                ans += ["\033[93m", str(guess[index])]
+            else :
+                ans += ["\033[91m", str(guess[index])]
+        elif level <= 3 :
+            if guess[index] == answer[index] :
+                ans += ["\033[92m", str(guess[index])]
+            elif guess[index] == answer[0] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[1] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[2] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[3] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[4] :
+                ans += ["\033[93m", str(guess[index])]
+            else :
+                ans += ["\033[91m", str(guess[index])]
+        elif level <= 5 :
+            if guess[index] == answer[index] :
+                ans += ["\033[92m", str(guess[index])]
+            elif guess[index] == answer[0] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[1] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[2] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[3] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[4] :
+                ans += ["\033[93m", str(guess[index])]
+            elif guess[index] == answer[5] :
+                ans += ["\033[93m", str(guess[index])]
+            else :
+                ans += ["\033[91m", str(guess[index])] 
         index += 1
     return ans
